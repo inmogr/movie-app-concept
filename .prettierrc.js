a3993046d3539470536b0c6b1d5c064f78c7663b
Morhaf Shamia <inmogr@msn.com>
2021-07-11 01:43:01 +0800

    updated prettier config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,22 @@
 module.exports = {
-  bracketSpacing: false,
-  jsxBracketSameLine: true,
-  singleQuote: true,
-  trailingComma: 'all',
-  arrowParens: 'avoid',
+    arrowParens: "always",
+    bracketSpacing: true,
+    endOfLine: "auto",
+    jsxBracketSameLine: false,
+    jsxSingleQuote: false,
+    proseWrap: "never",
+    semi: true,
+    singleQuote: false,
+    tabWidth: 4,
+    trailingComma: "all",
+    useTabs: false,
+    printWidth: 160,
+    overrides: [
+        {
+            files: "*.json",
+            options: {
+                printWidth: 10,
+            },
+        },
+    ],
 };
