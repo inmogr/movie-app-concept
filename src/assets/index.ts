5f2c0106ad70d054fcb1ac0619daf40094342d9e
Morhaf Shamia <inmogr@msn.com>
2021-07-12 01:10:22 +0800

    designed stars components

---
+++ src/assets/index.ts
@@ -0,0 +1,7 @@
+import star from "./star.svg";
+import star_outline from "./star_outline.svg";
+
+export const Asset = {
+    star,
+    star_outline,
+};
---
+++ src/components/RatingStars.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { View } from "react-native";
+
+import { Asset } from "../assets";
+
+export const convertRatingToStars = (rating?: number) => {
+    if (!rating) {
+        return 0;
+    }
+    return Math.floor((rating / 10) * 5);
+};
+
+export const getStarsArray = (rating?: number) => {
+    const count = convertRatingToStars(rating);
+    const stars = new Array(5).fill(0);
+    stars.fill(1, 0, count);
+    return stars;
+};
+
+export interface RatingStarsProps {
+    rating?: number;
+}
+
+const RatingStars = React.memo((props: RatingStarsProps) => {
+    return (
+        <React.Suspense fallback={null}>
+            {getStarsArray(props.rating).map((star, index) => (
+                <React.Fragment>
+                    {index ? <View style={{ width: 8 }} /> : null}
+                    {star ? <Asset.star height={32} width={32} /> : <Asset.star_outline height={32} width={32} />}
+                </React.Fragment>
+            ))}
+        </React.Suspense>
+    );
+});
+
+export default RatingStars;
