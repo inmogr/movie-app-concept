afdb446b1b39620ae2c15519b3c368ef5229f97a
Morhaf Shamia <inmogr@msn.com>
2021-07-12 00:31:57 +0800

    import without *

--- src/pages/Details.tsx
+++ src/pages/Details.tsx
@@ -1,5 +1,5 @@
 import { useNavigation } from "@react-navigation/core";
-import * as React from "react";
+import React from "react";
 import { Text, View } from "react-native";
 import { Button } from "react-native-paper";
 import { ROUTES } from "../constants";
